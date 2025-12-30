50f470b3285e08f5a9fb7a7fa3fed5f505a9a8a5
Kaedras <6783810+Kaedras@users.noreply.github.com>
2025-12-30 14:16:56 +0100

    Escape file name in `usvfsCreateProcessHooked()` command string

--- src/usvfsmanager.cpp
+++ src/usvfsmanager.cpp
@@ -426,7 +426,7 @@ pid_t UsvfsManager::usvfsCreateProcessHooked(const std::string& file,
     }
   }
 
-  const string cmd = file + " " + arg;
+  const string cmd = "'" + file + "' " + arg;
   logger::debug("{}: command string: {}", __FUNCTION__, cmd);
 
   int pipefd[2];
