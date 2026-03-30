8d072fea1d1b59f53f62351a9b30352867907179
Kaedras <6783810+Kaedras@users.noreply.github.com>
2026-03-30 14:19:03 +0200

    Improve documentation

--- src/utils.h
+++ src/utils.h
@@ -11,18 +11,30 @@ std::string toUpper(std::string_view str) noexcept;
 void toUpperInplace(std::string& str) noexcept;
 void toLowerInplace(std::string& str) noexcept;
 
+/**
+ * @brief Extracts the file name from a given path.
+ **/
 std::string getFileNameFromPath(std::string_view path) noexcept;
 
 /**
  * @brief Returns the parent path of the provided path.
- * @note Returns empty string instead of "/"
+ * @note Returns empty string instead of "/".
  */
 std::string getParentPath(std::string_view path) noexcept;
 
+/**
+ * @brief Checks if `parentPath` is a parent directory of `path`.
+ */
 bool isParentPathOf(std::string_view parentPath, std::string_view path) noexcept;
 
-// functions similar to std::filesystem::relative, but without accessing the filesystem
-// does not check for errors and assumes that `p` is a subdirectory of `base`
+/**
+  @brief Functions similar to std::filesystem::relative, but does not access the
+filesystem.
+  @note Does not check for errors and assumes that `p` is a subdirectory of `base`.
+**/
 std::string_view relativePath(std::string_view p, std::string_view base) noexcept;
 
+/**
+ * @brief Creates a string to describe the flags passed to `open()`.
+ */
 std::string openFlagsToString(int flags) noexcept;
